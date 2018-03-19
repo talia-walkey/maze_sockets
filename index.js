d4fc375bb0bb0f1b1eb526dba68c2d1efc1a826d
talia-walkey <twrox8@gmail.com>
2018-03-19 16:25:28 -0700

    finished maze

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const server = require('http').Server();
 var io = require('socket.io')(server);
 
 var allnames = [];
+var chosenFruit = '';
+var theWinner = '';
+
 io.on('connection', function(socket){
     console.log('user has connected');
     
@@ -14,6 +17,36 @@ io.on('connection', function(socket){
         console.log(allnames);
         
         io.emit('allnames', allnames);
+    })
+    
+    socket.on('chosenF', function(data){
+        console.log('new fruit is '+ data);
+        
+        chosenFruit = data;
+        
+        console.log('chosen fruit is '+chosenFruit);
+        
+        io.emit('chosenFruit', chosenFruit);
+    })
+    
+    socket.on('playerF', function(data){
+        console.log('players fruit is '+ data);
+        
+        chosenFruit = data;
+        
+        console.log('chosen fruit is '+chosenFruit);
+        
+        io.emit('chosenFruit', chosenFruit);
+    })
+    
+    socket.on('theWin', function(data){
+        console.log('the winner is '+ data);
+        
+        theWinner = data;
+        
+        console.log('chosen fruit is '+theWinner);
+    
+        io.emit('theWinner', theWinner);
     })
     
     socket.on('disconnect', function(){
